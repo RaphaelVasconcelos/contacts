bf6029bba80a24d570d499548d55ebacc2df9076
raphael.vasconcelos <raphael.vasconcelos01@gmail.com.br>
2020-04-13 11:18:29 -0300

    Adicionando novos detalhes a splash view

--- lib/android/views/splash.view.dart
+++ lib/android/views/splash.view.dart
@@ -5,6 +5,30 @@ class SplashView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+          ),
+          Icon(
+            Icons.fingerprint,
+            size: 72,
+            color: Theme.of(context).accentColor,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Meus contatos",
+            style: TextStyle(
+              fontSize: 24,
+              color: Theme.of(context).accentColor,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
