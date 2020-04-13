ae96b957681a4fea7808dd23b38134b3b31d5f75
raphael.vasconcelos <raphael.vasconcelos01@gmail.com.br>
2020-04-13 11:45:32 -0300

    Adicionando icones na home view e pagina de detalhes do contato

--- lib/android/views/home.view.dart
+++ lib/android/views/home.view.dart
@@ -1,3 +1,5 @@
+import 'package:contacts/android/views/details.view.dart';
+// import 'package:contacts/android/views/editor-contact.view.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -11,7 +13,58 @@ class HomeView extends StatelessWidget {
         centerTitle: true,
         leading: FlatButton(
           onPressed: () {},
-          child: Icon(Icons.search, color: Theme.of(context).primaryColor),
+          child: Icon(
+            Icons.search,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(48),
+                image: DecorationImage(
+                  image:
+                      NetworkImage("https://balta.io/imgs/andrebaltieri.jpg"),
+                ),
+              ),
+            ),
+            title: Text("André Baltieri"),
+            subtitle: Text("11 97222-7742"),
+            trailing: FlatButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailsView(),
+                  ),
+                );
+              },
+              child: Icon(
+                Icons.chat,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailsView(),
+            ),
+          );
+        },
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).accentColor,
         ),
       ),
     );
