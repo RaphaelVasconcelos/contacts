a78d5c94daffbff89509445d55a9b9fce7733569
raphael.vasconcelos <raphael.vasconcelos01@gmail.com.br>
2020-04-13 11:07:45 -0300

    Criando tela de abertura do android

--- lib/android/android.app.dart
+++ lib/android/android.app.dart
@@ -1,4 +1,5 @@
 import 'package:contacts/android/styles.dart';
+import 'package:contacts/android/views/splash.view.dart';
 import 'package:flutter/material.dart';
 
 class AndroidApp extends StatelessWidget {
@@ -8,9 +9,7 @@ class AndroidApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Contacts',
       theme: androidTheme(),
-      home: Scaffold(
-        body: Text("OK"),
-      ),
+      home: SplashView(),
     );
   }
 }
